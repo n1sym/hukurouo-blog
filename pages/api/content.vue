3eb5bfde210060fc36cd13033e57e2fc4231ef7b
hukurouo <owlbook248@gmail.com>
2020-12-08 00:34:25 +0900

    偽api

---
+++ pages/api/content.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    {{ articles }}
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData ({ $content, params }) {
+    const query = await $content('articles' || 'index').only(['title', 'path']).sortBy('date', 'desc')
+    const articles = await query.fetch()
+    return {articles}
+  }
+}
+</script>
+
+<style>
+
+</style>
